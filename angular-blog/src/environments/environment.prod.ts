14f7109d6d89fe46ea48f4b114b26cad3b8d5730
Kasun Suess <ks_ext@marktplatz-agentur.de>
2020-10-29 14:44:42 +0200

    mod

---
+++ angular-blog/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import { Environment } from "./interface";
+
+export const environment: Environment = {
+  production: true,
+  apiKey: "AIzaSyDiTmTKVr5S9rwgilb8FbKEc5LGa0YRSKw",
+  fbDbUrl: "https://angular-shop-adeef.firebaseio.com",
+};
